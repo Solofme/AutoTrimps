10e13635cf7c8670374f877e30727940e7ef3aba
Solofme <solofme@gmail.com>
2022-01-10 20:49:26 +0100

    Revert "Changing URLs"
    This reverts commit b960a1aa4902c3bc4bc835a02a8e3dbe9b75c077.

--- .user.js
+++ .user.js
@@ -8,7 +8,6 @@
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
 // @connect      *Zorn192.github.io/AutoTrimps*
-// @connect      *raw.githubusercontent.com/Solofme/AutoTrimps/*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        GM_xmlhttpRequest 
@@ -17,8 +16,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Zek';
 //This can be edited to point to your own Github Repository URL.
-// script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
-script.src = 'https://raw.githubusercontent.com/Solofme/AutoTrimps/Tests/AutoTrimps2.js';
+script.src = 'https://Solofme.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
