e6ed7a341d213c880a9e1ef206d556bdb753c424
Solofme <solofme@gmail.com>
2022-01-16 19:28:24 +0100

    Simple linear approach to increasing prestige settings.

--- modules/dynprestige.js
+++ modules/dynprestige.js
@@ -1,37 +1,85 @@
-function prestigeChanging2(){var a=document.getElementById('Prestige').selectedIndex;if(!(2>=a)){var b=getPageSetting('DynamicPrestige2'),c=10<a?a-10:0,d=0;for(i=1;i<=a;i++){var e=game.mapUnlocks[autoTrimpSettings.Prestige.list[i]].last;if(e<=b-5){var g=Math.floor((b-e)/5);4<=game.global.sLevel&&(g=Math.ceil(g/2)),d+=g}}'Lead'==game.global.challengeActive&&(d*=2);var h=0;return 0==d?void(autoTrimpSettings.Prestige.selected=document.getElementById('Prestige').value):void(h=Math.ceil(d/a),game.global.world>b-h&&(game.global.mapBonus<a?!0==game.global.slowDone?autoTrimpSettings.Prestige.selected='GambesOP':autoTrimpSettings.Prestige.selected='Bestplate':game.global.mapBonus>a&&(autoTrimpSettings.Prestige.selected='Dagadder')),(game.global.world<=b-h||10==game.global.mapBonus)&&(autoTrimpSettings.Prestige.selected='Dagadder'))}}
-function RprestigeChanging2(){
-    var maxPrestigeIndex = document.getElementById('RPrestige').selectedIndex;
-    if (maxPrestigeIndex <= 2)
-        return;
-    var lastzone = getPageSetting("RDynamicPrestige2");
-    var extra = maxPrestigeIndex > 10 ? maxPrestigeIndex - 10 : 0;
-    var neededPrestige = 0;
-    for (i = 1; i <= maxPrestigeIndex ; i++){
-        var lastp = game.mapUnlocks[autoTrimpSettings.RPrestige.list[i]].last;
-        if (lastp <= lastzone - 5){
-            var rem = lastzone - lastp;
-            var addto = Math.floor(rem/5);
-            if (game.global.sLevel >= 4)
-                addto = Math.ceil(addto/2);
-            neededPrestige += addto;
-        }
-    }
-    var zonesToFarm = 0;
-    if (neededPrestige == 0){
-        autoTrimpSettings.RPrestige.selected = document.getElementById('RPrestige').value;
-        return;
-    }
-    zonesToFarm = Math.ceil(neededPrestige/maxPrestigeIndex);
-    if(game.global.world > (lastzone-zonesToFarm)){
-        if (game.global.mapBonus < maxPrestigeIndex) {
-            if(game.global.slowDone == true)
-                autoTrimpSettings.RPrestige.selected = "GambesOP";
-            else
-                autoTrimpSettings.RPrestige.selected = "Bestplate";
-        }
-        else if (game.global.mapBonus > maxPrestigeIndex)
-             autoTrimpSettings.RPrestige.selected = "Dagadder";
-    }
-    if (game.global.world <= lastzone-zonesToFarm || game.global.mapBonus == 10)
-        autoTrimpSettings.RPrestige.selected = "Dagadder";
+function prestigeChanging2() {
+	var prestSelected = document.getElementById('Prestige').selectedIndex;
+
+	if (!(prestSelected <= 2)) {
+		var prestAvail = autoTrimpSettings.Prestige.list - (game.global.slowDone ? 1 : 3);
+		var tarWorld = getPageSetting('DynamicPrestige2');
+		var worldsLeft = game.global.world - tarWorld;
+
+		// Simple linear
+		var dagadderIdx = 2;
+		var worldsPerStep = tarWorld / ((prestSelected - dagadderIdx) / 2);
+		var calcWorld = Math.min(tarWorld, game.global.world);
+		autoTrimpSettings.Prestige.selected = autoTrimpSettings.Prestige.list[dagadderIdx + Math.round(calcWorld / worldsPerStep) * 2];
+	}
+}
+
+		//var numPrestiges = 0;
+		//for (ii = 1; ii <= prestSelected; ii++) {
+		//	var lastPrestige = game.mapUnlocks[autoTrimpSettings.Prestige.list[ii]].last;
+		//	// Upgrade available?
+		//	if (lastPrestige + 5 <= tarWorld) {
+		//		numPrestiges += Math.floor((tarWorld - lastPrestige) / 5);
+		//	}
+		//}
+
+		//if (numPrestiges == 0) {
+		//	autoTrimpSettings.Prestige.selected = document.getElementById('Prestige').value;
+		//	return;
+		//}
+
+		//if ('Lead' == game.global.challengeActive)
+		//	numPrestiges *= 2;
+		//if (game.global.sLevel >= 4)
+		//	numPrestiges /= 2;
+
+		//var averageMissing = Math.ceil(missingPrest / prestSelected);
+		//if (game.global.world > tarWorld - averageMissing) {
+		//	if (game.global.mapBonus < prestSelected) {
+		//		if (game.global.slowDone)
+		//			autoTrimpSettings.Prestige.selected = 'GambesOP';
+		//		else
+		//			autoTrimpSettings.Prestige.selected = 'Bestplate';
+		//	}
+		//} else {
+		//	autoTrimpSettings.Prestige.selected = 'Dagadder';
+		//}
+//	}
+//}
+
+function RprestigeChanging2() {
+	var maxPrestigeIndex = document.getElementById('RPrestige').selectedIndex;
+	if (maxPrestigeIndex <= 2)
+		return;
+	var lastzone = getPageSetting("RDynamicPrestige2");
+	var extra = maxPrestigeIndex > 10 ? maxPrestigeIndex - 10 : 0;
+	var neededPrestige = 0;
+	for (ii = 1; ii <= maxPrestigeIndex; ii++) {
+		var lastp = game.mapUnlocks[autoTrimpSettings.RPrestige.list[ii]].last;
+		if (lastp <= lastzone - 5) {
+			var rem = lastzone - lastp;
+			var addto = Math.floor(rem / 5);
+			if (game.global.sLevel >= 4)
+				addto = Math.ceil(addto / 2);
+			neededPrestige += addto;
+		}
+	}
+	var zonesToFarm = 0;
+	if (neededPrestige == 0) {
+		autoTrimpSettings.RPrestige.selected = document.getElementById('RPrestige').value;
+		return;
+	}
+	zonesToFarm = Math.ceil(neededPrestige / maxPrestigeIndex);
+	if (game.global.world > (lastzone - zonesToFarm)) {
+		if (game.global.mapBonus < maxPrestigeIndex) {
+			if (game.global.slowDone == true)
+				autoTrimpSettings.RPrestige.selected = "GambesOP";
+			else
+				autoTrimpSettings.RPrestige.selected = "Bestplate";
+		}
+		else if (game.global.mapBonus > maxPrestigeIndex)
+			autoTrimpSettings.RPrestige.selected = "Dagadder";
+	}
+	if (game.global.world <= lastzone - zonesToFarm || game.global.mapBonus == 10)
+		autoTrimpSettings.RPrestige.selected = "Dagadder";
 }
